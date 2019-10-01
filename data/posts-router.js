dee47290dd4232955189cde843ab70539f98b7d4
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-10-01 15:54:13 -0500

    able to get posts by id and comments for a specific post

--- data/posts-router.js
+++ data/posts-router.js
@@ -47,7 +47,7 @@ router.post('/:id/comments', (req, res) => {
         }
     })
     
-})
+});
 
 router.get('/', (req, res) => {
     Posts.find()
@@ -56,6 +56,40 @@ router.get('/', (req, res) => {
     })
     .catch(err => {
         res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
+});
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+
+    Posts.findById(id)
+    .then(post => {
+        if(post.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+
+            res.json(post)            
+        }
+    })
+    .catch (err => {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+});
+
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id
+
+    Posts.findPostComments(id)
+    .then(post => {
+        if(post.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.json(post)
+        }
+    })
+    .catch(err => {
+        res.status(500).json("The comments information could not be retrieved.")
     })
 })
 
